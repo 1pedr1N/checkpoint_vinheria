05ed22c25dc8dd7ebb50cb6d5807611a72eda0e9
Pedro Silva <88340056+1pedr1N@users.noreply.github.com>
2023-03-31 21:01:58 -0300

    :fire:

---
+++ README.md
@@ -0,0 +1,5 @@
+Pedro Henrique Silva de Morais - RM98804
+luisa Cristina dos Santos Neves - RM551889
+Lucas Rodrigues Delfino - RM550196
+Gabriel Aparecido Cassalho Xavier - RM99794
+Gustavo Carmo Santana Souza -RM98279
---
+++ pages/somaPage/script.js
@@ -0,0 +1,7 @@
+function soma(){
+    num1 = Number(prompt('Digite o primeiro valor:'))
+    num2 = Number(prompt('Digite o segundo numero:'))
+    const somando = num1 + num2 
+    alert(`O valor da soma é ${somando}`)
+}
+soma()
