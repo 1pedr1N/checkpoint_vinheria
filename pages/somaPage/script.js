cb5905dd646a431c3900feb15e2e3735bed00c4e
Pedro Silva <88340056+1pedr1N@users.noreply.github.com>
2023-05-05 21:32:43 -0300

    :lock:

--- pages/somaPage/script.js
+++
@@ -1,7 +0,0 @@
-function soma(){
-    num1 = Number(prompt('Digite o primeiro valor:'))
-    num2 = Number(prompt('Digite o segundo numero:'))
-    const somando = num1 + num2 
-    alert(`O valor da soma é ${somando}`)
-}
-soma()
